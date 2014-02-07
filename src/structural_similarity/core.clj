0980864e34da495f30fc7a557c8de6110eed831d
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-07 14:05:43 -0500

    refactored and removed conds. won't handle similar? routines

--- src/structural_similarity/core.clj
+++ src/structural_similarity/core.clj
@@ -1,22 +1,31 @@
 (ns structural-similarity.core
   (:require [clj-http.client :as client]
-            [structural-similarity.edit-distance :as tree-edit-distance]))
+            [structural-similarity.edit-distance :as tree-edit-distance]
+            [structural-similarity.schema :as schema]))
 
 (defn page-body
   [a-link]
   (try (-> a-link client/get :body)
        (catch Exception e nil)))
 
+(def *algorithms-functions-similarity*
+  {:tree-edit-distance tree-edit-distance/similarity
+   :page-schema        schema/similarity})
+
+(def *algorithms-functions-similar?*
+  {:tree-edit-distance tree-edit-distance/similar?
+   :page-schema        schema/similar?})
+
 (defn similarity
   "Algorithm must be one of:
    :tree-edit-distance"
   [doc1 doc2 algorithm]
-  (cond (= algorithm :tree-edit-distance)
-        (tree-edit-distance/similarity doc1 doc2)
-        
-        :else
-        (throw
-         (UnsupportedOperationException. "Algorithm not implemented"))))
+  (let [algorithms (set
+                    (keys *algorithms-functions-similarity*))]
+    (if (some #{algorithm} algorithms)
+      ((*algorithms-functions-similarity* algorithm) doc1 doc2)
+      (throw
+       (UnsupportedOperationException. "Algorithm not implemented")))))
 
 (defn similarity-link
   "For convenience, fetch pages from the specified URIs
@@ -29,9 +38,8 @@
 
 (defn similar?
   [doc1 doc2 algorithm]
-  (cond (= algorithm :tree-edit-distance)
-        (<= 0.8 (similarity doc1 doc2 algorithm))
-
-        :else
-        (throw
-         (UnsupportedOperationException. "Algorithm not implemented"))))
+  (let [algorithms (set (keys *algorithms-functions-similar?*))]
+    (if (some #{algorithm} algorithms)
+      ((*algorithms-functions-similar?* algorithm) doc1 doc2)
+      (throw
+       (UnsupportedOperationException. "Algorithm not implemented")))))
