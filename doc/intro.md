6c1d4baefc7b7bcfca749defdad15247f9a88da0
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-06 20:30:34 -0500

    initial commit with BSD license

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to structural_similarity
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/structural_similarity/core_test.clj
@@ -0,0 +1,7 @@
+(ns structural-similarity.core-test
+  (:require [clojure.test :refer :all]
+            [structural-similarity.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
