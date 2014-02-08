6f3af274343fcbed844f42b18362acade40d0a59
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-07 20:24:08 -0500

    lein test now dumps a sexy report

--- test/structural_similarity/core_test.clj
+++ test/structural_similarity/core_test.clj
@@ -1,8 +1,10 @@
 (ns structural-similarity.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [me.raynes.fs :as fs]
             [structural-similarity.core :refer :all]
-            [structural-similarity.dataset :as dataset]))
+            [structural-similarity.dataset :as dataset])
+  (:use [clojure.pprint :only [pprint]]))
 
 (def *algorithms* (keys *algorithms-functions-similarity*))
 
@@ -11,31 +13,33 @@
   (when-not (fs/exists? dataset/*corpus-file*)
     (dataset/build-dataset)))
 
+(defn write-report
+  [report]
+  (pprint report (io/writer "resources/report.clj")))
+
 (deftest similarity-test
   (do
     (initialize)
-    (let [dataset (dataset/read-dataset)]
-      (doall
-       (doseq [{name   :name
-                truth  :similar?
-                links  :links
-                corpus :corpus}
-               dataset]
-         
-         (print
-          (map
-           (fn [[ls docs]]
-             (let [[l1   l2]   ls
-                   [doc1 doc2] docs]
-               (map
-                (fn [algorithm]
-                  (let [computed (similar? doc1 doc2 algorithm)]
-                    (println :algorithm algorithm)
-                    (println :link1 l1)
-                    (println :link2 l2)
-                    (println :expected truth)
-                    (println :computed computed)
-                   (is (= truth computed))))
-                *algorithms*)))
-           (map vector links corpus))))))))
+    (let [dataset (dataset/read-dataset)
+          report  (map
+                   (fn [algorithm]
+                     {algorithm
+                      (map
+                       (fn [{name   :name
+                            truth  :similar?
+                            links  :links
+                            corpus :corpus}]
+                         (map
+                          (fn [[ls docs]]
+                            (let [[l1 l2]     ls
+                                  [doc1 doc2] docs]
+                              {:name      name
+                               :link1     l1
+                               :link2     l2
+                               :expected  truth
+                               :computed  (similar? doc1 doc2 algorithm)}))
+                          (map vector links corpus)))
+                       dataset)})
+                   *algorithms*)]
+      (write-report report))))
 
