e7fae1effa5925778b15f8d35d5e23af21441915
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-22 15:17:52 -0500

    edit distance report

--- edit-distance-report.clj
+++ edit-distance-report.clj
@@ -1,35 +1,63 @@
-{:global {:fail 16, :success 45},
- :accuracy 0.7377049180327869,
+{:global {:fail 10, :success 61},
+ :accuracy 0.8591549295774648,
  :per-corpus
- ([:negatives {:success 25}]
-  [:forum-leaf-similar-page-pairs {:success 2, :fail 7}]
+ ([:qa-similar-index-page-pairs {:success 5}]
+  [:negatives {:success 30}]
+  [:forum-leaf-similar-page-pairs {:success 3, :fail 6}]
   [:forum-index-similar-page-pairs {:success 9}]
-  [:blog-leaf-similar-page-pairs {:success 5, :fail 4}]
-  [:blog-index-similar-page-pairs {:success 4, :fail 5}])}
-{:negatives
+  [:blog-leaf-similar-page-pairs {:fail 2, :success 7}]
+  [:blog-index-similar-page-pairs {:success 7, :fail 2}])}
+{:qa-similar-index-page-pairs
+ ({:link1 "http://stackoverflow.com/questions",
+   :link2 "http://stackoverflow.com/questions?page=2&sort=newest",
+   :expected true,
+   :computed true,
+   :info 0.9775175644028103}
+  {:link1 "http://mindthebook.com/",
+   :link2 "http://mindthebook.com/questions/search/popular/page/2",
+   :expected true,
+   :computed true,
+   :info 0.8302325581395349}
+  {:link1 "http://askville.amazon.com/Index.do",
+   :link2
+   "http://askville.amazon.com/Index.do?page=2&filter=AllAnswersOnly",
+   :expected true,
+   :computed true,
+   :info 0.9923878205128205}
+  {:link1 "http://superuser.com/questions",
+   :link2 "http://superuser.com/questions?page=2&sort=newest",
+   :expected true,
+   :computed true,
+   :info 0.966275659824047}
+  {:link1 "http://www.metafilter.com/",
+   :link2 "http://www.metafilter.com/index.cfm?page=2",
+   :expected true,
+   :computed true,
+   :info 0.9024833434282253}),
+ :negatives
  ({:link1 "http://blog.shriphani.com/",
    :link2
    "http://blog.shriphani.com/2013/12/18/robust-principal-component-pursuit-background-matrix-recovery/",
    :expected false,
    :computed false,
-   :info 0.07350835322195703}
+   :info 0.6188001889466226}
   {:link1 "http://rottenindenmark.wordpress.com/",
    :link2
    "http://rottenindenmark.wordpress.com/2012/02/29/what-is-your-boss-buying/",
    :expected false,
    :computed false,
-   :info 0.5019762845849802}
+   :info 0.5032281205164993}
   {:link1 "http://carsandetc.tumblr.com/",
    :link2
    "http://carsandetc.tumblr.com/post/76160507825/chevy-camaro-z-28",
    :expected false,
    :computed false,
-   :info 0.675120244862265}
+   :info 0.6748251748251748}
   {:link1 "http://www.adventuresoncehad.com/",
    :link2 "http://www.adventuresoncehad.com/post/76190425016",
    :expected false,
    :computed false,
-   :info 0.491566265060241}
+   :info 0.4857142857142857}
   {:link1 "http://minimsft.blogspot.com/",
    :link2
    "http://minimsft.blogspot.com/2013/08/steve-ballmer-is-going-to-frickin.html",
@@ -47,48 +75,48 @@
    "http://paulmullins.wordpress.com/2014/02/08/domesticating-dissent-consuming-johnny-cash/",
    :expected false,
    :computed false,
-   :info 0.628226249313564}
+   :info 0.5752490823282643}
   {:link1 "http://sportsethicist.com/",
    :link2
    "http://sportsethicist.com/2014/02/05/boycotts-and-the-sochi-olympics/",
    :expected false,
    :computed false,
-   :info 0.4723646723646724}
+   :info 0.4638084632516704}
   {:link1 "http://techcrunch.com/",
    :link2
    "http://techcrunch.com/2014/02/10/inmobi-launches-interactive-video-ad-platform-to-help-advertisers-boost-sales-on-mobile/",
    :expected false,
    :computed false,
-   :info 0.2871319880913}
+   :info 0.2982817869415808}
   {:link1 "http://www.vbulletin.org/forum/forumdisplay.php?f=263",
    :link2 "http://www.vbulletin.org/forum/showthread.php?t=254967",
    :expected false,
    :computed false,
-   :info 0.1917454858125537}
+   :info 0.1912521440823328}
   {:link1
    "http://forums.finalgear.com/finalgear-meets/finalgear-lemon-party-texas-march-57401/",
    :link2 "http://forums.finalgear.com/the-site-itself/",
    :expected false,
    :computed false,
-   :info 0.1482398239823982}
+   :info 0.1587586560656579}
   {:link1 "http://www.theverge.com/forums",
    :link2
    "http://www.theverge.com/2014/2/6/5387804/looking-for-an-inexpensive-device-with-nfc",
    :expected false,
    :computed false,
-   :info 0.2308188265635074}
+   :info 0.2443686006825939}
   {:link1
    "http://www.12allchat.com/forum/viewtopic.php?f=143&t=120886",
    :link2 "http://www.12allchat.com/forum/viewforum.php?f=143",
    :expected false,
    :computed false,
-   :info 0.2328269484808454}
+   :info 0.2327500825354903}
   {:link1 "http://www.bmwfaq.com/forums/foro-general-bmw.7/",
    :link2
    "http://www.bmwfaq.com/threads/hoy-he-echo-un-60-a-320-km-conometrado-en-8-segundos.787580/",
    :expected false,
    :computed false,
-   :info 0.2207171314741036}
+   :info 0.2154803578374173}
   {:link1 "http://forums.asterisk.org/viewtopic.php?f=38&t=87887",
    :link2 "http://forums.asterisk.org/viewforum.php?f=38&start=50",
    :expected false,
@@ -99,30 +127,30 @@
    "http://www.reddit.com/r/aww/comments/1xitir/my_neighbor_took_a_picture_of_my_dogs_and_it/",
    :expected false,
    :computed false,
-   :info 0.1865316901408451}
+   :info 0.1898201985866357}
   {:link1 "http://blog.shriphani.com/",
    :link2
    "http://blog.shriphani.com/2014/01/11/all-it-took-was-an-aha/",
    :expected false,
    :computed false,
-   :info 0.1376228775692583}
+   :info 0.2574430823117338}
   {:link1 "http://rottenindenmark.wordpress.com/",
    :link2
    "http://rottenindenmark.wordpress.com/2014/02/01/bleg-anyone-been-to-bangladesh/",
    :expected false,
    :computed false,
-   :info 0.4953203743700504}
+   :info 0.4971264367816092}
   {:link1 "http://carsandetc.tumblr.com/",
    :link2
    "http://carsandetc.tumblr.com/post/76473121817/a-stunning-camaro-ss-convertible-sandwiched-in",
    :expected false,
    :computed false,
-   :info 0.6825817860300619}
+   :info 0.6771929824561403}
   {:link1 "http://www.adventuresoncehad.com/",
    :link2 "http://www.adventuresoncehad.com/post/76340631742",
    :expected false,
    :computed false,
-   :info 0.4619164619164619}
+   :info 0.4563106796116505}
   {:link1 "http://minimsft.blogspot.com/",
    :link2
    "http://minimsft.blogspot.com/2013/08/steve-ballmer-is-going-to-frickin.html",
@@ -140,19 +168,48 @@
    :link2 "http://paulmullins.wordpress.com/",
    :expected false,
    :computed false,
-   :info 0.6278814489571899}
+   :info 0.5752490823282643}
   {:link1 "http://sportsethicist.com/",
    :link2
    "http://sportsethicist.com/2014/02/10/ethics-of-high-school-athletic-transfers/",
    :expected false,
    :computed false,
-   :info 0.6523125996810207}
+   :info 0.64609375}
   {:link1 "http://techcrunch.com/",
    :link2
    "http://techcrunch.com/2014/02/12/white-house-unveils-cybersecurity-plan-for-big-firms-looks-to-silicon-valley-next/",
    :expected false,
    :computed false,
-   :info 0.3204134366925065}),
+   :info 0.3310450038138825}
+  {:link1 "http://stackoverflow.com/questions",
+   :link2
+   "http://stackoverflow.com/questions/21898464/windows-7-smartcard-service-hang-after-a-lot-of-scardcontrol",
+   :expected false,
+   :computed false,
+   :info 0.338544798556825}
+  {:link1 "http://mindthebook.com/",
+   :link2
+   "http://mindthebook.com/question/what_is_the_story_of_darryl_flicking",
+   :expected false,
+   :computed false,
+   :info 0.260655737704918}
+  {:link1 "http://askville.amazon.com/Index.do",
+   :link2
+   "http://askville.amazon.com/republican-resistance-legislation-help-create-jobs-based-sound-economics/AnswerViewer.do?requestId=90037687",
+   :expected false,
+   :computed false,
+   :info 0.2047165077772203}
+  {:link1 "http://superuser.com/questions",
+   :link2
+   "http://superuser.com/questions/719239/itunes-playback-skipping-on-some-songs",
+   :expected false,
+   :computed false,
+   :info 0.3368883312421581}
+  {:link1 "http://www.metafilter.com/",
+   :link2 "http://www.metafilter.com/136813/Venezuela-Riots",
+   :expected false,
+   :computed false,
+   :info 0.3351698806244261}),
  :forum-leaf-similar-page-pairs
  ({:link1 "http://www.vbulletin.org/forum/showthread.php?t=254967",
    :link2 "http://www.vbulletin.org/forum/showthread.php?t=295438",
@@ -169,21 +226,21 @@
    :link2 "http://forums.finalgear.com/problems/nsfw-subforum-57124/",
    :expected true,
    :computed false,
-   :info 0.6100200400801603}
+   :info 0.551849166062364}
   {:link1
    "http://www.theverge.com/2014/2/6/5387804/looking-for-an-inexpensive-device-with-nfc",
    :link2
    "http://www.theverge.com/2014/2/6/5386736/has-there-been-any-indication-of-a-north-american-z1-compact",
    :expected true,
    :computed true,
-   :info 0.9922958397534669}
+   :info 0.9922720247295209}
   {:link1
    "http://www.bmwfaq.com/threads/hoy-he-echo-un-60-a-320-km-conometrado-en-8-segundos.787580/",
    :link2
    "http://www.bmwfaq.com/threads/como-dice-el-anuncio-me-he-comprado-un-polo.786907/",
    :expected true,
    :computed true,
-   :info 0.847046031170714}
+   :info 0.8535701341065604}
   {:link1 "http://forum.avantbrowser.com/viewtopic.php?f=9&t=33057",
    :link2 "http://forum.avantbrowser.com/viewtopic.php?f=9&t=32452",
    :expected true,
@@ -199,7 +256,7 @@
   {:link1 "http://forums.asterisk.org/viewtopic.php?f=38&t=87887",
    :link2 "http://forums.asterisk.org/viewtopic.php?f=38&t=88051",
    :expected true,
-   :computed false,
+   :computed true,
    :info 0.7308176100628931}
   {:link1
    "http://www.artofproblemsolving.com/Forum/viewtopic.php?f=300&t=573898",
@@ -207,35 +264,35 @@
    "http://www.artofproblemsolving.com/Forum/viewtopic.php?f=300&t=23215",
    :expected true,
    :computed false,
-   :info 0.6744540375825292}),
+   :info 0.6753048780487805}),
  :forum-index-similar-page-pairs
  ({:link1 "http://www.vbulletin.org/forum/forumdisplay.php?f=263",
    :link2 "http://www.vbulletin.org/forum/forumdisplay.php?f=26",
    :expected true,
    :computed true,
-   :info 0.9392165691130122}
+   :info 0.939380332285586}
   {:link1 "http://forums.finalgear.com/the-site-itself/",
    :link2 "http://forums.finalgear.com/top-gear-america/",
    :expected true,
    :computed true,
-   :info 0.8466290625839377}
+   :info 0.8484685652874798}
   {:link1 "http://forums.finalgear.com/fifth-gear/",
    :link2 "http://forums.finalgear.com/top-gear-america/",
    :expected true,
    :computed true,
-   :info 0.9311111111111111}
+   :info 0.9274395329441201}
   {:link1 "http://www.theverge.com/forums",
    :link2 "http://www.theverge.com/forums/all/forum/2",
    :expected true,
    :computed true,
-   :info 0.9991850040749796}
+   :info 0.9982502187226597}
   {:link1
    "http://www.artofproblemsolving.com/Forum/viewforum.php?f=300&",
    :link2
    "http://www.artofproblemsolving.com/Forum/viewforum.php?f=300&start=40",
    :expected true,
    :computed true,
-   :info 0.9687697160883281}
+   :info 0.9680124223602484}
   {:link1 "http://forums.asterisk.org/viewforum.php?f=38",
    :link2 "http://forums.asterisk.org/viewforum.php?f=38&start=50",
    :expected true,
@@ -246,7 +303,7 @@
    "http://www.12allchat.com/forum/viewforum.php?f=143&start=75",
    :expected true,
    :computed true,
-   :info 0.9935828877005348}
+   :info 0.9930518439337253}
   {:link1 "http://forum.avantbrowser.com/viewforum.php?f=9",
    :link2 "http://forum.avantbrowser.com/viewforum.php?f=9&start=120",
    :expected true,
@@ -256,28 +313,28 @@
    :link2 "http://www.bmwfaq.com/forums/foro-general-bmw.7/page-6",
    :expected true,
    :computed true,
-   :info 0.8883139284174213}),
+   :info 0.8845198675496689}),
  :blog-leaf-similar-page-pairs
  ({:link1
    "http://blog.shriphani.com/2014/01/11/all-it-took-was-an-aha/",
    :link2 "http://blog.shriphani.com/2013/12/26/the-sistine-chapel/",
    :expected true,
-   :computed false,
-   :info 0.7007874015748031}
+   :computed true,
+   :info 0.7181467181467182}
   {:link1
    "http://rottenindenmark.wordpress.com/2014/02/01/bleg-anyone-been-to-bangladesh/",
    :link2
    "http://rottenindenmark.wordpress.com/2014/01/30/five-stories-about-sports-for-people-who-hate-sports/",
    :expected true,
    :computed true,
-   :info 0.9798619102416571}
+   :info 0.9793459552495697}
   {:link1
    "http://carsandetc.tumblr.com/post/76473121817/a-stunning-camaro-ss-convertible-sandwiched-in",
    :link2
    "http://carsandetc.tumblr.com/post/76472905072/chevy-impala-parked-roadside",
    :expected true,
    :computed true,
-   :info 0.9396039603960396}
+   :info 0.9406614785992218}
   {:link1 "http://www.adventuresoncehad.com/post/76340631742",
    :link2 "http://www.adventuresoncehad.com/post/74869511763",
    :expected true,
@@ -303,47 +360,47 @@
    "http://paulmullins.wordpress.com/2014/01/27/the-final-choice-the-materiality-of-last-meals/",
    :expected true,
    :computed false,
-   :info 0.6369661266568483}
+   :info 0.6074438202247191}
   {:link1
    "http://sportsethicist.com/2014/02/10/ethics-of-high-school-athletic-transfers/",
    :link2
    "http://sportsethicist.com/2014/02/05/boycotts-and-the-sochi-olympics/",
    :expected true,
    :computed false,
-   :info 0.697874784606548}
+   :info 0.6935665914221218}
   {:link1
    "http://techcrunch.com/2014/02/12/white-house-unveils-cybersecurity-plan-for-big-firms-looks-to-silicon-valley-next/",
    :link2
    "http://techcrunch.com/2014/02/06/apple-hiring-fitness-physiologists-sleep-experts-on-the-heels-of-recent-iwatch-rumors/?source=gravity",
    :expected true,
-   :computed false,
+   :computed true,
    :info 0.7931206380857428}),
  :blog-index-similar-page-pairs
  ({:link1 "http://blog.shriphani.com/",
    :link2 "http://blog.shriphani.com/index-2.html",
    :expected true,
    :computed false,
-   :info 0.3290816326530612}
+   :info 0.3618739903069467}
   {:link1 "http://rottenindenmark.wordpress.com/",
    :link2 "http://rottenindenmark.wordpress.com/2012/02/",
    :expected true,
-   :computed false,
-   :info 0.7838302752293578}
+   :computed true,
+   :info 0.7838534211279702}
   {:link1 "http://carsandetc.tumblr.com/",
    :link2 "http://carsandetc.tumblr.com/page/2",
    :expected true,
    :computed true,
-   :info 0.9794204115917682}
+   :info 0.968949383241174}
   {:link1 "http://www.adventuresoncehad.com/",
    :link2 "http://www.adventuresoncehad.com/page/2",
    :expected true,
    :computed true,
-   :info 0.9138755980861244}
+   :info 0.9008}
   {:link1 "http://minimsft.blogspot.com/",
    :link2
    "http://minimsft.blogspot.com/search?updated-max=2011-04-28T07:52:00-07:00&max-results=11",
    :expected true,
-   :computed false,
+   :computed true,
    :info 0.7740416946872898}
   {:link1 "http://lifeandartintheup.blogspot.com/",
    :link2
@@ -355,14 +412,14 @@
    :link2 "http://paulmullins.wordpress.com/page/2/",
    :expected true,
    :computed true,
-   :info 0.880236305048335}
+   :info 0.8419689119170984}
   {:link1 "http://sportsethicist.com/",
    :link2 "http://sportsethicist.com/2013/07/",
    :expected true,
-   :computed false,
-   :info 0.7582005623242737}
+   :computed true,
+   :info 0.7649219467401286}
   {:link1 "http://techcrunch.com/",
    :link2 "http://techcrunch.com/page/2/",
    :expected true,
    :computed true,
-   :info 0.9601528384279476})}
+   :info 0.9586455731334999})}
